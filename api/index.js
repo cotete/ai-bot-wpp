215a7cc8bd5cdbec432aba8e2bfcf569632181e3
cotete <feliperibeirots@gmail.com>
2025-01-30 21:23:16 -0300

    tirada a mensagem de erro

--- api/index.js
+++ api/index.js
@@ -24,7 +24,6 @@ if (!apiKey || !mongoURI) {
 async function connectToMongoDB() {
     try {
         await mongoose.connect(mongoURI, {
-            useNewUrlParser: true,
             useUnifiedTopology: true
         });
         console.log('Conectado ao MongoDB');
